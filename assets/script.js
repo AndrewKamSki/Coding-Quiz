04e4810d3266c7f977415b05b1d3b8b1a852bbe6
AKam <andy.kaminski42@gmail.com>
2022-08-10 09:21:59 -0400

    Added comments and minor fixes to display

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,28 @@
+// Answer buttons
+var answerBtns = document.querySelector("#answers");
 var ans1btn = document.querySelector("#ans1");
 var ans2btn = document.querySelector("#ans2");
 var ans3btn = document.querySelector("#ans3");
 var ans4btn = document.querySelector("#ans4");
 var quizQuestions = document.querySelector("#quiz-questions");
-var answerBtns = document.querySelector("#answers");
-var startBtn = document.querySelector("#start-btn");
+// Prompts/ outputs
 var timerEl = document.querySelector(".timer-count");
 var scoreSubmitEl = document.querySelector("#submit-entry");
 var scoresEl = document.querySelector(".scores");
 var initialsEl = document.querySelector("#initials");
+var ansResultEl = document.querySelector(".ans-result");
+// Navigation buttons
+var startBtn = document.querySelector("#start-btn");
 var submitBtn = document.querySelector("#submit")
 var goBackBtn = document.querySelector("#go-back");
 var clearScoresBtn = document.querySelector("#clear-highscores");
-var ansResultEl = document.querySelector(".ans-result");
-
+// initial variables
 var quizArraySelect = 0;
 var timer;
 var timerCount;
 var finalTime;
 var initials;
-
+// blank array for logging and posting scores
 var pastScores = [];
 
 // Array of objects with each object being a question and giving its possible answers
@@ -70,27 +73,38 @@ var quizArray = [
     ]
   }
 ];
-
+// starts on page load
 function init() {
+  quizQuestions.textContent = '';
   quizQuestions.setAttribute("class", "center");
   startBtn.setAttribute("class", "btn");
   goBackBtn.setAttribute("class", "hidden");
   clearScoresBtn.setAttribute("class", "hidden");
-  ansResultEl.setAttribute("class", "hidden")
-}
+  ansResultEl.setAttribute("class", "hidden");
+  // creates opening page prompt
+  var h1El = document.createElement("h1");
+  var pEl = document.createElement("p");
+  h1El.textContent = "Coding Quiz Challenge";
+  pEl.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your scoreline by ten seconds."
+  h1El.setAttribute("class", "center");
+  pEl.setAttribute("class", "center");
+  quizQuestions.appendChild(h1El);
+  quizQuestions.appendChild(pEl)
 
+}
+// Starts the quiz
 function startQuiz() {
   timerCount = 75;
   startTimer();
   startBtn.setAttribute("class", "hidden");
-  answerBtns.setAttribute("class", "buttons");
+  answerBtns.setAttribute("class", "buttons center");
   setQuestions();
 }
-
+// Runs the currently selected set of questions and answers from the array
 function setQuestions() {
   renderQuestions(quizArray[quizArraySelect]);
 }
-
+// Creates the questions and answers on the screen
 function renderQuestions(question) {
   quizQuestions.textContent = question.question;
   ans1btn.textContent = question.answers[0].answerText;
@@ -98,15 +112,17 @@ function renderQuestions(question) {
   ans3btn.textContent = question.answers[2].answerText;
   ans4btn.textContent = question.answers[3].answerText;
 }
-
+// Checks if answer is correct
 function checkAnswer(e) {
   // evaluation of if clicked button was correct
   var clickedBtn = e.target.textContent;
+  // runs through each option in the quiz array for the correct answer and compares the selected with the correct one
   for (var i=0;i<quizArray[quizArraySelect].answers.length;i++) {
     if(clickedBtn == quizArray[quizArraySelect].answers[i].answerText) {
       var answerBool = quizArray[quizArraySelect].answers[i].answerResult;
     }
   }
+  // subtract time if answered incorrectly
   if (!answerBool){
     timerCount = timerCount - 10;
   }
@@ -124,7 +140,7 @@ function checkAnswer(e) {
     setQuestions();
   }
 }
-
+// Initializes the timer
 function startTimer(){
   timer = setInterval(function() {
     timerCount--;
@@ -136,31 +152,31 @@ function startTimer(){
     }
   }, 1000);
 };
-
+// Makes the enter score prompt to log high scores
 function enterScore() {
   timerEl.textContent = " ";
   answerBtns.setAttribute("class", "hidden");
   quizQuestions.textContent = "All Done! You scored a " + finalTime + "!";
   scoreSubmitEl.setAttribute("class", "submit-score center");
 }
-
+// Sends scores to local storage
 function setScores() {
   var entry = {
     initials: initialsEl.value,
     score: finalTime
   }
+  // Checks if entry already in local storage
   if (pastScores !== null) {
     pastScores.push(entry);
   } else {
+    // If local storage already full, adds new object to stored array
     storedScores = localStorage.getItem("pastScores").split(',');
     storedScores.push(entry);
-    storedScores.sort();
-    console.log(storedScores);
   }
   localStorage.setItem("pastScores", JSON.stringify(pastScores));
   getScores();
 }
-
+// Retrieves scores from local storage
 function getScores() {
   scoreSubmitEl.setAttribute("class", "hidden");
   goBackBtn.setAttribute("class", "btn");
@@ -171,8 +187,12 @@ function getScores() {
   answerBtns.setAttribute("class", "hidden");
   quizQuestions.textContent= "High Scores";
   var storedScores = JSON.parse(localStorage.getItem("pastScores"));
+  // Check if local storage is empty
   if (storedScores !== null) {
+    // Sorts scores highest to lowest
+    storedScores.sort((a,b) => b.score - a.score);
     ansResultEl.textContent = '';
+    // Prints the scores to the screen
     for (i=0;i<storedScores.length;i++) {
       var val = storedScores[i].initials + ": " + storedScores[i].score;
       ansResultEl.innerHTML += val + '<br/>';
@@ -181,7 +201,7 @@ function getScores() {
     return;
   }
 };
-
+// Removes past scores from screen and local storage
 function clearHighscores () {
   localStorage.clear();
   pastScores = [];
@@ -207,5 +227,5 @@ scoresEl.addEventListener("click", getScores);
 // Event listener to clear past scores
 clearScoresBtn.addEventListener("click", clearHighscores);
 
-// Initializes the page and retrieves past scores
+// Starts on page load
 init();
